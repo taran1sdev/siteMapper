2deda30ffbd66b0a8d8e75e097d546de8d618d7f
taran1sdev <thomasairddev@gmail.com>
2025-04-09 20:42:12 +0100

    Added Functionality: Performs a bfs on input url, user can specify depth, encodes links to xml sitemap format and prints to command-line

--- siteMapper.go
+++ siteMapper.go
@@ -8,16 +8,79 @@ import (
 	"strings"
 	"link/link"
 	"io"
+	"encoding/xml"
+	"os"
 )
 
-var inputUrl = flag.String("url", "www.google.com", "The root of the domain to clone")
+var inputUrl = flag.String("url", "https://www.google.com", "The root of the domain to map")
+var maxDepth = flag.Int("depth", 2, "The maximum amount of links to follow")
+
+// datatype for creating <loc></loc> tags in xml output
+
+type loc struct {
+	Loc string `xml:"loc"`  
+}
+
+// datatype for outer xml tags
+
+type urlset struct {
+	Urls []loc `xml:"url"`
+	Xmlns string `xml:"xmlns,attr"`
+}
 
 func main() {
 	flag.Parse()
 	
-	links := get(*inputUrl)
-	fmt.Println(links)
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("  ", "    ")
+
+	links := bfs(*inputUrl, *maxDepth)
+	
+	toXml := urlset{
+		Xmlns:"http://www.sitemaps.org/schemas/sitemap/0.9",
+	}
+
+	for _, link := range links {
+		toXml.Urls = append(toXml.Urls, loc{Loc:link})
+	}
+	
+	if err := enc.Encode(toXml); err != nil{
+		fmt.Println("Error when encoding to xml", err)
+	}
+	fmt.Println()
 }
+
+// Function performs a breadth-first-search on all local links found in website
+
+func bfs(urlString string, depth int) []string {
+	seen := make(map[string]struct{})
+	var queue map[string]struct{}
+	nextQueue := map[string]struct{}{
+		urlString: struct{}{},
+	}
+	for i := 0; i < depth; i++ {
+		queue, nextQueue = nextQueue, make(map[string]struct{})
+		if len(queue) == 0{
+			break
+		}
+		for url, _ := range queue {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				nextQueue[link] = struct{}{}
+			}
+		}
+	}
+	links := make([]string, 0, len(seen))
+	for url, _ := range seen{
+		links = append(links, url)
+	}
+	return links
+}
+
+// Function performs a http GET requests to get html content to Parse
 
 func get(link string)  []string {
 	// Get the html response
@@ -49,7 +112,7 @@ func filter(links []string, keepFn func(string) bool) (filtered []string) {
 	return
 }
 
-// Function returns a function that checks prefix of given string
+// Function returns a function that checks prefix of given string - For checking if links are local and start with http || /
 
 func withPrefix(pfx string) func(string) bool {
 	return func (link string) bool {
